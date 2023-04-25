71641b2d7a7f7b8b032a1c48877c6388a89f27e8
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-25 14:05:40 +0530

    Name modified, areEquals to equalityCheckBy

--- test/test-framework-test.js
+++ test/test-framework-test.js
@@ -2,45 +2,49 @@ const demo = require("../src/demo.js");
 const testLibrary = require("../lib/testing.js");
 const displayHeadline = testLibrary.displayHeadline;
 const assertEquals = testLibrary.assertEquals;
-const isEqualsTo = testLibrary.isEqualsTo;
-const areEquals = testLibrary.areEquals;
+const equalityCheckBy = testLibrary.equalityCheckBy;
 const displaySummary = testLibrary.displaySummary;
 
-const testAreEquals = function() {
+const testEqualityCheckBy = function() {
   const it = function(testName, testData) {
     assertEquals(testData.actual, testData.expected, testName);
   };
 
-  displayHeadline("testAreEquals");
+  displayHeadline("testEqualityCheckBy");
 
   it("Should give true, as number provided is same", {
-    actual: areEquals(3, 3),
+    actual: equalityCheckBy(3, 3),
     expected: true
   });
 
+  it("Should give false, as number provided is not same", {
+    actual: equalityCheckBy(3, 4),
+    expected: false
+  });
+
   it("Should give true, as text provided is same", {
-    actual: areEquals("hi", "hi"),
+    actual: equalityCheckBy("hi", "hi"),
     expected: true
   });
 
   it("Should give false, as text provided is not same", {
-    actual: areEquals("hi", "h"),
+    actual: equalityCheckBy("hi", "h"),
     expected: false
   });
 
   it("Should give true, as array provided is same", {
-    actual: areEquals([1], [1]),
+    actual: equalityCheckBy([1], [1]),
     expected: true 
   });
 
   it("Should give true, as nested array provided is same", {
-    actual: areEquals([[2]], [[2]]),
+    actual: equalityCheckBy([[2]], [[2]]),
     expected: true 
   });
 };
 
 const test = function() {
-  testAreEquals();
+  testEqualityCheckBy();
 };
 
 test();
